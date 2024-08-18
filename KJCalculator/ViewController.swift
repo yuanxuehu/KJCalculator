cb0a22d06f6192d2901b3fa0396c41f20fccc81b
TigerHu <yuanxuehu2016@126.com>
2024-08-18 22:09:30 +0800

    Initial Commit

---
+++ KJCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KJCalculator
+//
+//  Created by TigerHu on 2024/8/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
